9e9c1c0693a894d5a1e32632b3667c2a184ace1b
jrobischon <jrobischon@gmail.com>
2018-05-21 09:27:20 -0600

    dedupe and clean dataset

---
+++ data_clean.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+if __name__ == "__main__":
+
+    # Import data
+    df = pd.read_csv("data/wiki_movie_plots.csv")
+
+    # Title is not Null
+    df = df[df["Title"].notnull()]
+
+    # Remove newlines from Title
+    df["Title"] = df["Title"].apply(lambda x: x.replace("\n", " "))
+
+    df["Genre"].fillna("Unknown", inplace=True)
+    df["Genre"] = df["Genre"].apply(lambda x: x.lower().replace("\n", " "))
+
+    df["Director"].fillna("Unknown", inplace=True)
+    df["Director"] = df["Director"].apply(lambda x: x.replace("\n", " "))
+
+    # Dedupe
+    df = df.groupby(["Title", "Origin/Ethnicity", "Director"], as_index=False).first()
+
+    # Save as csv
+    df.to_csv("data/wiki_movie_plots_deduped.csv", index=False)
